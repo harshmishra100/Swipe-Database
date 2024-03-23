56f54b9762337d08b7263bfbf3e55bd0fb5baf90
harshmishra100 <145322566+harshmishra100@users.noreply.github.com>
2024-03-24 02:10:09 +0530

    Update indexex.sql

---
+++ indexex.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_accounttype ON login(Accounttype);
+CREATE INDEX idx_industry ON startups_information(industry);
+CREATE INDEX idx_interested_field ON investor_interests(interested_field);
+CREATE INDEX idx_chattype ON chats(type);
