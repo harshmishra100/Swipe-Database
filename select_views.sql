516e9157a850c236fbf7d24a275e1c63f2eb6fcd
harshmishra100 <145322566+harshmishra100@users.noreply.github.com>
2024-03-24 02:11:26 +0530

    Update select_views.sql

---
+++ select_views.sql
@@ -0,0 +1,26 @@
+-- Select query for investor_details view
+SELECT * FROM investor_details;
+
+-- Select query for investor_interests_view view
+SELECT * FROM investor_interests_view;
+
+-- Select query for startup_details view
+SELECT * FROM startup_details;
+
+-- Select query for startup_team_view view
+SELECT * FROM startup_team_view;
+
+-- Select query for recent_messages_one_on_one view
+SELECT * FROM recent_messages_one_on_one;
+
+-- Select query for recent_messages_group_chat view
+SELECT * FROM recent_messages_group_chat;
+
+-- Select query for all_messages_details view
+SELECT * FROM all_messages_details;
+
+-- Select query for SwipeDetails view
+SELECT * FROM SwipeDetails;
+
+-- Select query for SwipeDetailsSuccess view
+SELECT * FROM SwipeDetailsSuccess;
